b77e0847a3ea8630f49fd07a6d1d54949dffb99b
nhonmc <136806287+nhonmc@users.noreply.github.com>
2023-06-21 15:32:18 +0700

    Create nhonmc.1bt.uk.js

---
+++ domains/nhonmc.1bt.uk.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Minecraft Group',
+  domain: '1bt.uk',
+  subdomain: 'nhonmc',
+  owner: {
+    repo: 'https://github.com/nhonmc/nhonmc.github.io',
+    email: 'canhngu023@gmail.com',
+  },
+  record: {
+    CNAME: 'nhonmc.github.io'
+  },
+  proxy: false,
+})
